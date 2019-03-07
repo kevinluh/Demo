ef681a7d14de39afaff5ff465b1dfaea8246b6c1
Kevin Luh <kevin955217@hotmail.com>
2019-03-07 12:47:08 -0500

    Remove pin files

--- src/components/CityPin.js
+++ src/components/CityPin.js
@@ -1,7 +1,5 @@
 import React, {PureComponent} from 'react';
 
-// import {ReactComponent as PIN} from './MapPin.svg';
-
 const PIN = "M8 2.1c1.1 0 2.2 0.5 3 1.3 0.8 0.9 1.3 1.9 1.3 3.1s-0.5 2.5-1.3 3.3l-3 3.1-3-3.1c-0.8" +
         "-0.8-1.3-2-1.3-3.3 0-1.2 0.4-2.2 1.3-3.1 0.8-0.8 1.9-1.3 3-1.3z";
 const pinStyle = {
